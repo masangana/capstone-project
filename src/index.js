0e2cde19047a34b6dcd01dbfcad68a3d6811af37
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-21 23:47:08 +0100

    Started the popup of the reservation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
-import "./style.scss";
-//import './style.css';
+import './style.scss';
+// import AddReservation from './modules/AddReservation.js';
+import EventListerners from './modules/ReservationEventListener';
+
+EventListerners.buttonSubmit()
+// AddReservation.buttonSubmit()
+// AddReservation.getDataToUse()
